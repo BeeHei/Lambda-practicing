f5e99e1cb4cf600a4d2e0f5a85790364356bd9e6
BKei <rock.illia@gmail.com>
2024-03-23 11:21:06 +0200

    Lambda declaration varitations

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,27 @@
-
 fun main() {
 
     val coffeeMachine = CoffeeMachine()
 
     val name = "latte"
-    val filter: (String) -> Unit = {argName: String ->
+    val filter: (String) -> Unit = { argName: String ->
         println("Filtered $argName")
     }
 
     coffeeMachine.prepareToDrink(name, filter)
+
+    coffeeMachine.prepareToDrink(name, { println("Filtered $it") })
+
+    coffeeMachine.prepareToDrink(name) { println("Filtered $it") }
 }
 
-    class CoffeeMachine{
+class CoffeeMachine {
 
-        fun prepareToDrink(name: String, myFilter: (String) -> Unit){
-            myFilter.invoke("$name.")
-            println("Prepared $name.")
-        }
+    fun prepareToDrink(
+        name: String,
+        myFilter: (String) -> Unit
+    ) {
+        myFilter.invoke("$name.")
+        println("Prepared $name.")
     }
+}
 
